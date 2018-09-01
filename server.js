5bbef16cf2eddfdc044c7aa5c4b65ca9be65e018
Roland Shen <rolandtshen@gmail.com>
2018-09-01 13:54:05 +0200

    Add EJS, change filenames

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const request = require('request');
 const argv = require('yargs').argv;
 
+//Express server
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+
+
 let apiKey = "4282babee20db9c3497ea2d3f298e357"
 let city = argv.c || "San Francisco"
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
